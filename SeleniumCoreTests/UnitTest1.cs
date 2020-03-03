e61b14a8e7898f66795decb16c9cb81d06fe2c97
Tiemi Matsumoto <tiemi.matsumoto@gmail.com>
2020-03-03 16:04:13 +0000

    Initial project structure - first commit

---
+++ SeleniumCoreTests/UnitTest1.cs
@@ -0,0 +1,21 @@
+using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumCoreTests
+{
+    [TestClass]
+    public class MainPage
+    {
+        [TestMethod]
+        public void ShouldLogin()
+        {
+            var driverDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var driver = new ChromeDriver(driverDirectory);
+            driver.Navigate().GoToUrl("https://www.saucedemo.com");
+
+        }
+    }
+}
